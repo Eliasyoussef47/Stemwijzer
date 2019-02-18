2bfc569e5baa4140c49151efef0e7b32d72e18c6
Elias Youssef <d-s-a00@hotmail.com>
2019-02-18 11:45:24 +0100

    changeHslColor toegevoegd

--- Colors Tools.js
+++ Colors Tools.js
@@ -1,6 +1,18 @@
 function getMinInObject(object) {
     
 }
+function getObjectKeyName(object, objectProperty) {
+    return Object.keys(object).find(function(element) {
+        return object[element] === objectProperty;
+    });
+}
+function objectToArray(object) {
+    let arr = [];
+    for (let key in object) {
+        arr.push(object[key]);
+    }
+    return arr;
+}
 function getElementBackgroundColor(element) {
     return window.getComputedStyle(element).backgroundColor;
 }
@@ -8,10 +20,10 @@ function getColorChannelsArrayFromRgb(rgb) {
     let rgbArray = rgb.slice(rgb.indexOf("(") + 1, rgb.length - 1).replace(/\s/g, '').split(",");
     rgbArray.forEach(function (element, index) {
         rgbArray[index] = Number(element);
-    })
+    });
     return rgbArray;
 }
-function getColorChannelsObjectFromRgb(rgb/*rgba(255, 0, 0, 0.5) of array met kanalen*/) {
+function getColorChannelsObjectFromRgb(rgb) {/*rgba(255, 0, 0, 0.5) of array met kanalen*/
     let colorsObj = {};//de object die terug gegeven gaat worden
     let rgbArray = rgb; // de array met de kanalen (ik ga vanuit dat een array gepasst wordt)
     if (!Array.isArray(rgb)) {//als rgb geen array is maak ik een array van
@@ -28,24 +40,60 @@ function getColorChannelsObjectFromRgb(rgb/*rgba(255, 0, 0, 0.5) of array met ka
     return colorsObj;
 }
 
-function rgbaToHsla(rgbaObject) {
-    if (Array.isArray(rgbaObject) || typeof rgbaObject === 'string') {
-        rgbaObject = getColorChannelsObjectFromRgb(rgbaObject);
-    }
-    for (var key in rgbaObject) {
-        rgbaObject[key] = Math.round((rgbaObject[key] / 255) * 100) / 100;
-    }
-}
+// function rgbaToHsla(rgbaObject) {
+//     if (Array.isArray(rgbaObject) || typeof rgbaObject === 'string') {
+//         rgbaObject = getColorChannelsObjectFromRgb(rgbaObject);
+//     }
+//     for (var key in rgbaObject) {
+//         rgbaObject[key] = Math.round((rgbaObject[key] / 255) * 100) / 100;
+//     }
+// }
 function rgbaToHsla(rgbaArray) {
-    rgbaArray.for(function (element, index) {
-        rgbaArray[index] = Math.round((element / 255) * 100) / 100;
+    let h, s, l;
+    let a = rgbaArray[3] || 1;
+    rgbaArray.forEach(function (element, index) {
+        rgbaArray[index] = element / 255;
     });
     let channelsNames = ["r", "g", "b", "a"];
-    let minChannel = Math.min(...rgbaArray);
-    let minChannelName = channelsNames[rgbaArray.indexOf(minChannel)];
-    let maxChannel = Math.max(...rgbaArray);
+    let minChannel = Math.min(rgbaArray[0], rgbaArray[1], rgbaArray[2]);
+    let maxChannel = Math.max(rgbaArray[0], rgbaArray[1], rgbaArray[2]);
     let maxChannelName = channelsNames[rgbaArray.indexOf(maxChannel)];
+    l = (minChannel + maxChannel) / 2;
+    if (minChannel === maxChannel) {
+        s = 0;
+    } else {
+        if (l < 0.5) {
+            s = (maxChannel - minChannel) / (maxChannel + minChannel);
+        } else if (l >= 0.5) {
+            s = (maxChannel - minChannel) / (2 - maxChannel - minChannel);
+        }
+    }
+    if (maxChannelName === "r") {
+        h = (rgbaArray[1] - rgbaArray[2]) / (maxChannel - minChannel);
+    } else if (maxChannelName === "g") {
+        h = 2 + (rgbaArray[2] - rgbaArray[0]) / (maxChannel - minChannel);
+    } else if (maxChannelName === "b") {
+        h = 4 + (rgbaArray[0] - rgbaArray[1]) / (maxChannel - minChannel);
+    }
+    h = h * 60;
+    return  "hsla(" + (Math.round(h)) + ", " + (Math.round(s * 100)) + "%, " + (Math.round(l * 100)) + "%, " + a + ")";
 }
 
-
+function changeHslColor(hsl, channel, opperation, factor) {
+    let hslArray = hsl.slice(hsl.indexOf("(") + 1, hsl.length - 1).replace(/\s/g, '').split(",");
+    let a = hslArray[3] || 1;
+    let channelIndex = ["h", "s", "l", "a"];
+    hslArray[1] = hslArray[1].replace("%", ""); hslArray[2] = hslArray[2].replace("%", "");
+    hslArray.forEach(function (element, index) {
+        hslArray[index] = Number(element);
+    });
+    if (opperation === "+") {
+        hslArray[channelIndex.indexOf(channel)] += factor;
+    } else if (opperation === "-") {
+        hslArray[channelIndex.indexOf(channel)] -= factor;
+    } else if (opperation === "=") {
+        hslArray[channelIndex.indexOf(channel)] = factor;
+    }
+    return  "hsla(" + hslArray[0] + ", " + hslArray[1] + "%, " + hslArray[2] + "%, " + a + ")";
+}
 
